b9e12f7d69fbf4a3106bf11d885420b9f28f70a3
huimin <huim.cao@gmail.com>
2022-07-31 23:44:38 +0200

    Add angular tasks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-tasks-test'`, () => {
+  it(`should have as title 'angular-tasks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-tasks-test');
+    expect(app.title).toEqual('angular-tasks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-tasks-test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-tasks app is running!');
   });
 });
