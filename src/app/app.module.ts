22fbd0c28dbff547af722502e869d4da69932d03
huimin <huim.cao@gmail.com>
2022-08-01 11:52:54 +0200

    Finish route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core"
 import { BrowserModule } from "@angular/platform-browser"
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
 import { AppRoutingModule } from "./app-routing.module"
+import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from "@angular/common/http"
 import { FormsModule } from "@angular/forms"
 
@@ -10,11 +11,18 @@ import { HeaderComponent } from "./components/header/header.component"
 import { ButtonComponent } from "./components/button/button.component"
 import { TasksComponent } from "./components/tasks/tasks.component"
 import { TaskItemComponent } from "./components/task-item/task-item.component"
-import { AddTaskComponent } from "./components/add-task/add-task.component"
+import { AddTaskComponent } from "./components/add-task/add-task.component";
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component'
+
+const routes: Routes = [  
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent, TaskItemComponent, AddTaskComponent],
-  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, HttpClientModule, FormsModule],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent, TaskItemComponent, AddTaskComponent, AboutComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule, HttpClientModule, FormsModule, RouterModule.forRoot(routes, {enableTracing:true})],
   providers: [],
   bootstrap: [AppComponent],
 })
